3b70405ea1719a329a71e1c2b16039a0871c9bba
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-11-05 20:16:59 +0100

    Roxygen update.

--- R/mkPrcp.R
+++ R/mkPrcp.R
@@ -1,7 +1,8 @@
-mkPrcp <- function(country, ref_ext = NULL, 
-                   start_date, end_date, 
-                   overall_limit = 1, monthly_limit = 0.5, valid_months = 1,
-                   p = 0.05, cores = 1L, visualize = TRUE) {
+mkPrcp <- function(country, dsn = getwd(), ref_ext = NULL, rds = NULL, chirps = NULL,
+                   start_date, end_date, interval = c("monthly", "bimonthly"),
+                   prewhitening = TRUE, overall_limit = 1, monthly_limit = 0.5, 
+                   valid_months = 1, 
+                   cores = 1L, visualize = TRUE, p = 0.05) {
   
   ## required packages
   library(rworldmap)
@@ -9,8 +10,8 @@ mkPrcp <- function(country, ref_ext = NULL,
   
   ## parallelize
   if (cores > 1L) {  
-    supcl <- parallel::makeCluster(cores)
-    doParallel::registerDoParallel(supcl)
+    cl <- parallel::makeCluster(cores)
+    doParallel::registerDoParallel(cl)
   }
   
   ## reference extent
@@ -24,16 +25,8 @@ mkPrcp <- function(country, ref_ext = NULL,
     stop("Please specify either a valid country name or a reference extent.\n")
   }
   
-  if (visualize)
-    plot(ref_ext)
-  
-  # download.file("http://biogeo.ucdavis.edu/data/gadm2.7/rds/IRN_adm0.rds", 
-  #               destfile = "data/IRN_adm0.rds")
-  # spy_iran <- readRDS("data/IRN_adm0.rds")
-  # suppressWarnings(proj4string(spy_iran) <- "+init=epsg:4326")
-  
   ## gsod stations in reference extent
-  gsod_shp <- gsodReformat(data = updateInventory(online = FALSE),
+  gsod_shp <- gsodReformat(data = updateInventory(),
                            elevation = TRUE, 
                            coords = TRUE,
                            df2sp = TRUE)
@@ -46,28 +39,26 @@ mkPrcp <- function(country, ref_ext = NULL,
   
   id_avl <- which(st <= start_date & nd >= end_date)
 
-  #   ## download data
-  #   suppressWarnings(
-  #     lst_gsod <- foreach(i = id_avl, .packages = "GSODTools") %dopar% {
-  #       usaf <- gsod_shp@data[i, "USAF"]
-  #       dlGsodStations(usaf = usaf, dsn = paste0("data/", usaf), unzip = TRUE, 
-  #                      start_year = substr(start_date, 1, 4), 
-  #                      end_year = substr(end_date, 1, 4))
-  #     }
-  #   )
-  
-  #   # save output
-  #   saveRDS(lst_gsod, file = "data/iran_1982_2013.rds")
-  lst_gsod <- readRDS("data/iran_1982_2013.rds")
-  
+  ## download data
+  if (!is.null(rds)) {
+    lst_gsod <- readRDS(rds)
+  } else {
+    suppressWarnings(
+      lst_gsod <- foreach(i = id_avl, .packages = "GSODTools") %dopar% {
+        usaf <- gsod_shp@data[i, "USAF"]
+        GSODTools::dlGsodStations(usaf = usaf, 
+                                  dsn = paste(dsn, usaf, sep = "/"), unzip = TRUE, 
+                                  start_year = substr(start_date, 1, 4), 
+                                  end_year = substr(end_date, 1, 4))
+      }
+    )
+  }
+
   ## stations including rainfall
   id_prcp <- sapply(lst_gsod, function(i) {
     "PRCP" %in% names(i)
   })
 
-  if (visualize)
-    points(gsod_shp[id_prcp, ], col = "grey75", pch = 4)
-  
   ## stations with sufficient amount of valid records
   dt_dates <- seq(start_date, end_date, 1)
   df_dates <- data.frame(YEARMODA = dt_dates)
@@ -86,64 +77,125 @@ mkPrcp <- function(country, ref_ext = NULL,
     lst_gsod <- lst_gsod[id_valid]
   }  
   
+  ## reject zero values when chirps > 0 mm
+  if (!is.null(chirps)) {
+    lst_gsod <- foreach(i = 1:length(lst_gsod)) %dopar% {
+      # gsod
+      dat_gsod <- lst_gsod[[i]]
+      dat_gsod$YEARMODA <- as.Date(as.character(dat_gsod$YEARMODA), 
+                                   format = "%Y%m%d")
+      
+      # chirps
+      dt_chirps <- as.Date(names(chirps)[1:sum(!is.na(names(chirps)))])
+      mat_chirps <- chirps[, 1:sum(!is.na(names(chirps)))]
+      dat_chirps <- data.frame(YEARMODA = dt_chirps, CHIRPS = mat_chirps[i, ])
+
+      # merge data by date
+      suppressWarnings(
+        dat <- merge(dat_gsod, dat_chirps, by = "YEARMODA", all.x = TRUE)      
+      )
+      dat$PRCP[dat$PRCP == 0 & dat$CHIRPS > 0] <- NA
+      
+      dat_gsod$PRCP <- dat$PRCP
+      return(dat_gsod)
+    }  
+  }
+  
+  
   # significant trends (p < 0.05); if not significant, NA is returned
   lst_dsn <- lapply(lst_gsod, function(i) {
     # merge station data with continuous time series
     i$YEARMODA <- as.Date(as.character(i$YEARMODA), format = "%Y%m%d")
     dat_mrg <- merge(df_dates, i, by = "YEARMODA", all = TRUE)
     
-    # aggregate by month
-    dat_mrg$YEARMO <- strftime(dat_mrg$YEARMODA, format = "%Y-%m")
+    ## aggregate by month
+    if (interval[1] == "monthly") {
+      dat_mrg$YEARMO <- strftime(dat_mrg$YEARMODA, format = "%Y-%m")
+      
+      dat_mrg %>%
+        dplyr::select(YEARMODA, YEARMO, PRCP) %>%
+        dplyr::mutate(PRCP = inch2Millimeter(PRCP, 2)) %>%
+        dplyr::group_by(YEARMO) %>%
+        dplyr::summarize(PRCP = GSODTools:::partialSum(PRCP, monthly_limit = monthly_limit))  %>%
+        data.frame() -> dat_agg
+      
+      dat_agg$MO <- sapply(strsplit(dat_agg$YEARMO, "-"), "[[", 2)    
+      
+    ## aggregate half months  
+    } else if (interval[1] == "bimonthly") {
 
-    dat_mrg %>%
-      dplyr::select(YEARMODA, YEARMO, PRCP) %>%
-      dplyr::group_by(YEARMO) %>%
-      dplyr::summarize(PRCP = GSODTools:::partialSum(PRCP, monthly_limit = monthly_limit))  %>%
-      data.frame() -> dat_agg
+      dat_mrg$YEARMO <- strftime(dat_mrg$YEARMODA, format = "%Y-%m")
+      
+      # determine first and second half per month
+      days <- sapply(1:nrow(dat_mrg), function(i) {
+        if (as.numeric(substr(dat_mrg$YEARMODA[i], 9, 10)) <= 15) {
+          return("15a") 
+        } else {
+          return("15b")
+        }
+      })
+      dat_mrg$YEARMO <- paste(dat_mrg$YEARMO, days, sep = "-")
+            
+      dat_mrg %>%
+        dplyr::select(YEARMODA, YEARMO, PRCP) %>%
+        dplyr::mutate(PRCP = inch2Millimeter(PRCP, 2)) %>%
+        dplyr::group_by(YEARMO) %>%
+        dplyr::summarize(PRCP = GSODTools:::partialSum(PRCP, monthly_limit = monthly_limit))  %>%
+        data.frame() -> dat_agg
+      
+      dat_agg$MO <- substr(dat_agg$YEARMO, 6, 11)    
+      
+    } 
 
-    dat_agg$MO <- sapply(strsplit(dat_agg$YEARMO, "-"), "[[", 2)    
-    
+    ## compute long-term (bi-)monthly means
     dat_agg %>% 
       dplyr::group_by(MO) %>%
       dplyr::summarize(PRCP_LTM = GSODTools:::partialMean(PRCP, valid_months = valid_months)) %>%
       data.frame() -> dat_ltm
     
+    ## compute seasonal (i.e., monthly or bi-monthly) anomalies
     dat_mrg <- merge(dat_agg, dat_ltm, by = "MO", sort = FALSE)
     dat_mrg <- dat_mrg[order(dat_mrg$YEARMO), ]
     dat_mrg$PRCP_DSN <- dat_mrg$PRCP - dat_mrg$PRCP_LTM
     
     return(dat_mrg)
   })
+  
+  # saveRDS(lst_dsn, file = "data/iran_dsn_1982_2013.rds")
 
-  num_tau <- sapply(lst_dsn, function(i) {  
+  lst_tau <- lapply(lst_dsn, function(i) {  
     # mann-kendall trend
-    mk <- Kendall::MannKendall(i$PRCP_DSN)
-    
-    # if trend is significant, return kendall's tau, else return NA
-    if (mk$sl < p) {
-      return(mk$tau)
-    } else {
-      return(NA)
-    }
+    Rsenal::significantTau(i$PRCP_DSN, prewhitening = prewhitening, 
+                           df = TRUE, p = p)
   })
-
+  dat_tau <- do.call("rbind", lst_tau)
+  
+  ## usaf codes
   ch_usaf <- sapply(lst_gsod, function(i) {
     unique(i[, 1])
   })
   
-  df_usaf <- data.frame(USAF = ch_usaf, TAU = num_tau)
+  df_usaf <- data.frame(USAF = ch_usaf, dat_tau)
   
   ## merge with spatial data
-  df_sign <- merge(updateInventory(), df_usaf, by = "USAF", all.y = TRUE)
-  coordinates(df_sign) <- ~ LON + LAT
-  proj4string(df_sign) <- "+init=epsg:4326"
+  df_usaf <- merge(updateInventory(), df_usaf, by = "USAF", all.y = TRUE)
+  coordinates(df_usaf) <- ~ LON + LAT
+  proj4string(df_usaf) <- "+init=epsg:4326"
   
   ## visualize
   if (visualize) {
-    points(df_sign, pch = 4)
-    points(subset(df_sign, !is.na(TAU) & TAU > 0), pch = 4, cex = 1.5, lwd = 1.5,
+    # reference extent
+    plot(ref_ext)
+    # stations with precipitation measurements (grey)
+    points(gsod_shp[id_prcp, ], col = "grey75", pch = 4)
+    # stations which match user-defined criteria (black)
+    points(df_usaf, pch = 4)
+    
+    # color significantly increasing (blue) and decreasing trends (red)
+    df_sign <- df_usaf[df_usaf@data$p < p, ]
+    points(subset(df_sign, !is.na(tau) & tau > 0), pch = 4, cex = 1.5, lwd = 2,
            col = "blue")
-    points(subset(df_sign, !is.na(TAU) & TAU < 0), pch = 4, cex = 1.5, lwd = 1.5,
+    points(subset(df_sign, !is.na(tau) & tau < 0), pch = 4, cex = 1.5, lwd = 2,
            col = "red")
   }
   
@@ -152,5 +204,5 @@ mkPrcp <- function(country, ref_ext = NULL,
     parallel::stopCluster(supcl)
   
   ## return mann-kendall data
-  return(df_sign)
+  return(df_usaf)
 }
